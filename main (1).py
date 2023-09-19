67b8036f655acca34dc0bf67ea85893bc278f125
Thavanithiya <144339365+Thavanithiya@users.noreply.github.com>
2023-09-19 10:51:10 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,49 @@
-def fact(n):
-  """this is a recursive function to find factorial of an integer"""
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact(n - 1)
+class Player:
 
-number = 5
-res = fact(number)
-print (" the factorial of {} is {}.". format(number, res))
+    def play(self):
+
+        print("The player is playing cricket.")
+
+
+
+
+
+class Batsman(Player):
+
+    def play(self):
+
+        print("The batsman is batting.")
+
+
+
+
+
+class Bowler(Player):
+
+    def play(self):
+
+        print("The bowler is bowling.")
+
+
+
+
+
+# Testing the Player, Batsman, and Bowler classes
+
+if __name__ == "__main__":
+
+    player = Player()
+
+    player.play()  # Output: The player is playing cricket.
+
+
+
+    batsman = Batsman()
+
+    batsman.play()  # Output: The batsman is batting.
+
+
+
+    bowler = Bowler()
+
+    bowler.play()  # Output: The bowler is bowling
